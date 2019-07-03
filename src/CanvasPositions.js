fa4b70424ba8bc69eb22b0ef983dc9b82d5c1103
Jim <jim_king_2000@163.com>
2019-07-03 20:43:34 +0800

    refactory

--- src/CanvasPositions.js
+++ src/CanvasPositions.js
@@ -1,28 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Marker } from 'react-amap';
 import { observer } from 'mobx-react';
 import { colorPool } from './common/PolylineStyle';
 import CarTopView from '../icon/CarTopView';
 
-export const CanvasPositions = observer(({ __map__, things, ...props }) => (
-  <>
-    {things && things.filter(p => p && p.latitude && p.longitude).map(p => (
-      <Marker
-        __map__={__map__}
-        key={p.thingId}
-        render={() => (<CarTopView
-          fontSize={30}
-          fill={colorPool[p.colorIndex % colorPool.length]}
-        />)}
-        title={p.isOnline ? '在线' : '离线'}
-        offset={[-15, -15]}
-        zIndex={150}
-        position={p}
-        angle={p.heading}
-        extData={p}
-        events={{ created: () => __map__.setFitView() }}
-        {...props}
-      />
-    ))}
-  </>
-));
+@observer
+export class CanvasPositions extends Component {
+  state = { selectedThing: null };
+  
+  render() {
+    const { __map__, things, ...props } = this.props;
+    return (
+      <>
+        {things && things.filter(p => p && p.latitude && p.longitude).map(p => (
+          <Marker
+            __map__={__map__}
+            key={p.thingId}
+            render={() => (<CarTopView
+              fontSize={30}
+              fill={colorPool[p.colorIndex % colorPool.length]}
+            />)}
+            title={p.isOnline ? '在线' : '离线'}
+            offset={[-15, -15]}
+            zIndex={150}
+            position={p}
+            angle={p.heading}
+            extData={p}
+            events={{
+              created: () => __map__.setFitView(),
+              click: e => this.setState({
+                selectedThing: e.target.getExtData()
+              })
+            }}
+            {...props}
+          />
+        ))}
+      </>
+    );
+  }
+}
