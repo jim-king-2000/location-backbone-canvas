1c4b24553b8c0c02c6a1a6ec911cc2add42170ba
Jim <jim_king_2000@163.com>
2019-07-04 14:13:12 +0800

    refactory

--- src/CanvasInformation.js
+++ src/CanvasInformation.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import { InfoWindow } from 'react-amap';
 import { observer } from 'mobx-react';
+import { Table, TableBody, TableRow, TableCell } from 'grommet';
 
 export const CanvasInformation = observer(({
-  __map__, onClose, data
+  __map__, onClose, data, template, ...props
 }) => (
   <InfoWindow
     __map__={__map__}
     position={data}
     visible={!!data}
     events={{ close: onClose }}
+    {...props}
   >
-    <div>abc</div>
+    {data && <Table>
+      <TableBody>
+        {template && template.map(t => (
+          <TableRow>
+            <TableCell>{t.label}</TableCell>
+            <TableCell>
+              {t.transform ?
+                t.transform(data[t.property]) :
+                data[t.property]}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>}
   </InfoWindow>
 ));
