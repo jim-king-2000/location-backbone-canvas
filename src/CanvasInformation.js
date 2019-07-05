0cc8d8a5931a02751acac14d7dd460a7565149e9
Jim <jim_king_2000@163.com>
2019-07-05 13:17:18 +0800

    refactory

--- src/CanvasInformation.js
+++ src/CanvasInformation.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { InfoWindow } from 'react-amap';
 import { observer } from 'mobx-react';
-import { Table, TableBody, TableRow, TableCell } from 'grommet';
+import { Box, Table, TableBody, TableRow, TableCell } from 'grommet';
 
 export const CanvasInformation = observer(({
   __map__, onClose, data, template, ...props
@@ -17,22 +17,24 @@ export const CanvasInformation = observer(({
       events={{ close: onClose }}
       {...props}
     >
-      <Table>
-        <TableBody>
-          {template && template.map(t => (
-            data[t.property] === undefined ?
-            undefined :
-            <TableRow key={t.property}>
-              <TableCell>{t.label}</TableCell>
-              <TableCell>
-                {t.transform ?
-                  t.transform(data[t.property]) :
-                  data[t.property]}
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <Box background={{ opacity: 'strong' }}>
+        <Table>
+          <TableBody>
+            {template && template.map(t => (
+              data[t.property] === undefined ?
+              undefined :
+              <TableRow key={t.property}>
+                <TableCell>{t.label}</TableCell>
+                <TableCell>
+                  {t.transform ?
+                    t.transform(data[t.property]) :
+                    data[t.property]}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Box>
     </InfoWindow>}
   </>
 ));
