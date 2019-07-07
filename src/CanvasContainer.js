d24680eb7ab325286cc732ce5bb2bedab7164982
Jim <jim_king_2000@163.com>
2019-07-07 22:49:18 +0800

    refactory

--- src/CanvasContainer.js
+++ src/CanvasContainer.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { Map } from 'react-amap';
 
 export const CanvasContainer = ({ children, ...props }) => (
-  <Map {...props}  plugins={['Scale']}
-    version='1.4.14'>
+  <Map plugins={['Scale']} version='1.4.14' {...props}>
     {children}
   </Map>
 );
