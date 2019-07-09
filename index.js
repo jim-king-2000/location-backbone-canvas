255dbf097adc637ac44473b3a45985d109763cdb
Jim <jim_king_2000@163.com>
2019-07-09 21:18:39 +0800

    add PluginZoom

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ export * from './src/CanvasContainer';
 export * from './src/CanvasInformation';
 export * from './src/CanvasPositions';
 export * from './src/CanvasTracks';
+export * from './src/PluginZoom';
