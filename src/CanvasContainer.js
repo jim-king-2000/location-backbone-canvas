61b9bdf5c8f1ebbc1f4b5dc1a29c37220cf68e18
Jim <jim_king_2000@163.com>
2019-07-10 21:01:57 +0800

    refactory

--- src/CanvasContainer.js
+++ src/CanvasContainer.js
@@ -5,6 +5,11 @@ import { Text } from 'grommet';
 export const CanvasContainer = ({ children, ...props }) => (
   <Map plugins={['Scale']} version='1.4.14' {...props}>
     {children}
-    <Text>版权所有&#169; 北京百特比邻信息科技有限公司</Text>
+    <Text style={{
+      position: 'absolute',
+      right: 0
+    }}>
+      版权所有&#169; 北京百特比邻信息科技有限公司
+    </Text>
   </Map>
 );
