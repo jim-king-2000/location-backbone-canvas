067d8c2e30d91140742aa9cf7034f11e6c2ee71f
Jim <jim_king_2000@163.com>
2019-07-10 21:04:41 +0800

    refactory

--- src/CanvasContainer.js
+++ src/CanvasContainer.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Map } from 'react-amap';
-import { Text } from 'grommet';
+import { Box, Text } from 'grommet';
 
 export const CanvasContainer = ({ children, ...props }) => (
   <Map plugins={['Scale']} version='1.4.14' {...props}>
     {children}
-    <Text style={{
+    <Box style={{
       position: 'absolute',
       right: 0
     }}>
-      版权所有&#169; 北京百特比邻信息科技有限公司
-    </Text>
+      <Text>版权所有&#169; 北京百特比邻信息科技有限公司</Text>
+    </Box>
   </Map>
 );
