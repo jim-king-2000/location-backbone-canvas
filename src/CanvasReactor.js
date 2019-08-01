ed49d497285ca3786d86a6e438b788c042b7e774
Jim <jim_king_2000@163.com>
2019-08-01 18:14:41 +0800

    finish CanvasReactor

--- src/CanvasReactor.js
+++ src/CanvasReactor.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 
 export class CanvasReactor extends Component {
   render() {
-    const markers = this.props.markers;
+    if (!this.props.tracingMode) return null;
+
     const __map__ = this.props.__map__;
     const bounds = __map__.getBounds();
-    const isMarkersInViewport = markers.filter(p => p.latitude && p.longitude).every(
+    const isMarkersInViewport = this.props.markers
+    .filter(p => p.latitude && p.longitude)
+    .every(
       p => bounds.contains(new AMap.LngLat(p.longitude, p.latitude))
     );
     if (!isMarkersInViewport) {
