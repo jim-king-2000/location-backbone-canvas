00254e8128f599bceb993db58508dd433b3c873c
Jim <jim_king_2000@163.com>
2019-08-01 16:21:39 +0800

    debug

--- src/CanvasReactor.js
+++ src/CanvasReactor.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 
 export class CanvasReactor extends Component {
   render() {
+    const markers = this.props.markers;
+    const __map__ = this.props.__map__;
+    const bounds = __map__.getBounds();
     console.log('Render CanvasReactor.');
+    console.log(bounds);
     return null;
   }
 }
