0b860886d2bf39cf7b94fe51ceb6a297e1a3a36b
Jim <jim_king_2000@163.com>
2019-08-01 16:10:46 +0800

    refactory

---
+++ src/CanvasReactor.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export class CanvasReactor extends Component {
+  render() {
+    return null;
+  }
+}
--- src/CanvasTracks.js
+++ src/CanvasTracks.js
@@ -1,30 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Polyline } from 'react-amap';
 import { lineStyle, colorPool } from './common/PolylineStyle';
 
-export class CanvasTracks extends Component {
-  render() {
-    const tracks = this.props.tracks;
-    const __map__ = this.props.__map__;
-    return (
-      <>
-        {tracks && tracks.map(track => {
-          let path = track.splittedTrack || [];
-          if (!Array.isArray(path) || !Array.isArray(path[0])) {
-            path = [path];
-          }
-          return path.map((pathItem, i) => <Polyline
-            __map__={__map__}
-            key={`${track.thingId}-${i}`}
-            path={pathItem}
-            events={{ created: () => __map__.setFitView() }}
-            style={{
-              strokeColor: colorPool[track.colorIndex % colorPool.length] ||
-                '#006600',
-              ...lineStyle
-            }} />);
-        })}
-      </>
-    );
-  }
-}
+export const CanvasTracks = ({ tracks, __map__ }) => (
+  <>
+    {tracks && tracks.map(track => {
+      let path = track.splittedTrack || [];
+      if (!Array.isArray(path) || !Array.isArray(path[0])) {
+        path = [path];
+      }
+      return path.map((pathItem, i) => <Polyline
+        __map__={__map__}
+        key={`${track.thingId}-${i}`}
+        path={pathItem}
+        events={{ created: () => __map__.setFitView() }}
+        style={{
+          strokeColor: colorPool[track.colorIndex % colorPool.length] ||
+            '#006600',
+          ...lineStyle
+        }} />);
+    })}
+  </>
+);
