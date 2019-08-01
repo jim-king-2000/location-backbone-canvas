8c921e6c5b626cb2a537c579544fad9db668dae2
Jim <jim_king_2000@163.com>
2019-08-01 16:16:34 +0800

    add CanvasReactor

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 export * from './src/CanvasContainer';
 export * from './src/CanvasInformation';
 export * from './src/CanvasPositions';
+export * from './src/CanvasReactor';
 export * from './src/CanvasTracks';
 export * from './src/PluginZoom';
--- src/CanvasReactor.js
+++ src/CanvasReactor.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 export class CanvasReactor extends Component {
   render() {
+    console.log('Render CanvasReactor.');
     return null;
   }
 }
