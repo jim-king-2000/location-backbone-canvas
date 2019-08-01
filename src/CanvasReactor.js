3731c4286bbf0a5582e50adfd13a7ac1ca0abfe3
Jim <jim_king_2000@163.com>
2019-08-01 17:35:52 +0800

    debug

--- src/CanvasReactor.js
+++ src/CanvasReactor.js
@@ -5,11 +5,11 @@ export class CanvasReactor extends Component {
     const markers = this.props.markers;
     const __map__ = this.props.__map__;
     const bounds = __map__.getBounds();
-    const isMarkersOutOfViewport = markers.filter(p => p.latitude && p.longitude).every(
+    const isMarkersInViewport = markers.filter(p => p.latitude && p.longitude).every(
       p => bounds.contains(new AMap.LngLat(p.longitude, p.latitude))
     );
     console.log(isMarkersOutOfViewport);
-    if (isMarkersOutOfViewport) __map__.setFitView();
+    if (!isMarkersInViewport) __map__.setFitView();
     return null;
   }
 }
