e487ce0b77a2ff1392c7ce0747d5346373502d57
Jim <jim_king_2000@163.com>
2019-08-05 20:41:32 +0800

    add CanvasPositionMonitor

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 export * from './src/CanvasContainer';
 export * from './src/CanvasInformation';
 export * from './src/CanvasPluginZoom';
+export * from './src/CanvasPositionMonitor';
 export * from './src/CanvasPositions';
 export * from './src/CanvasReactor';
 export * from './src/CanvasTracks';
