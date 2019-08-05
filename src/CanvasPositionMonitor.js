1d3c011759445ed992dea1e019d4aafe4b53e9da
Jim <jim_king_2000@163.com>
2019-08-05 21:15:36 +0800

    refactory

--- src/CanvasPositionMonitor.js
+++ src/CanvasPositionMonitor.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Box } from 'grommet';
 import { CanvasContainer } from './CanvasContainer';
 import { CanvasPositions } from './CanvasPositions';
 import { CanvasReactor } from './CanvasReactor';
@@ -22,32 +21,30 @@ export class CanvasPositionMonitor extends Component {
     const positions = this.props.positions;
     const selectThingId = this.props.selectThingId;
     return (
-      <Box flex={{ grow: 1, shrink: 1 }}>
-        <CanvasContainer mapkey={this.props.mapKey}>
-          <CanvasPositions
-            things={positions}
-            events={{
-              click: e => selectThingId(e.target.getExtData().thingId)
-            }}
-          />
-          <CanvasInformation
-            onClose={() => selectThingId(undefined) }
-            data={this.props.selectedThing}
-            template={this.props.propertyTemplate}
-          />
-          <CanvasPluginZoom
-            direction='row'
-            style={{
-              position: 'absolute',
-              top: 0
-            }}
-            tracingMode={this.state.tracingMode}
-            onChange={e => this.setState({ tracingMode: e.target.checked })} />
-          <CanvasReactor
-            markers={positions}
-            tracingMode={this.state.tracingMode} />
-        </CanvasContainer>
-      </Box>
+      <CanvasContainer mapkey={this.props.mapKey}>
+        <CanvasPositions
+          things={positions}
+          events={{
+            click: e => selectThingId(e.target.getExtData().thingId)
+          }}
+        />
+        <CanvasInformation
+          onClose={() => selectThingId(undefined) }
+          data={this.props.selectedThing}
+          template={this.props.propertyTemplate}
+        />
+        <CanvasPluginZoom
+          direction='row'
+          style={{
+            position: 'absolute',
+            top: 0
+          }}
+          tracingMode={this.state.tracingMode}
+          onChange={e => this.setState({ tracingMode: e.target.checked })} />
+        <CanvasReactor
+          markers={positions}
+          tracingMode={this.state.tracingMode} />
+      </CanvasContainer>
     );
   }
 }
