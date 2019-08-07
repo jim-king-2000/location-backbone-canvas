844189d27daf5271d94018b20a7f13f680cee6a6
Jim <jim_king_2000@163.com>
2019-08-07 12:08:45 +0800

    refactory

--- src/CanvasInformationTable.js
+++ src/CanvasInformationTable.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { observer } from 'mobx-react';
-import { Box, Table, TableBody, TableRow, TableCell } from 'grommet';
+import { Box, Table, TableHeader, TableBody, TableRow, TableCell } from 'grommet';
 
 export const CanvasInformationTable = observer(({ positions, template }) => (
   <Table>
