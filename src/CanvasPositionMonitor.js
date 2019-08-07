00f18ea129905d93599e6d685f0d3dad655a281e
Jim <jim_king_2000@163.com>
2019-08-07 12:53:38 +0800

    refactory

--- src/CanvasPositionMonitor.js
+++ src/CanvasPositionMonitor.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Box } from 'grommet';
 import { CanvasContainer } from './CanvasContainer';
 import { CanvasPositions } from './CanvasPositions';
 import { CanvasReactor } from './CanvasReactor';
