b20b2baf1c7b896ca6b1c0820c90c58928cdf5ea
Jim <jim_king_2000@163.com>
2019-08-20 21:50:09 +0800

    refactory

--- src/CanvasContainer.js
+++ src/CanvasContainer.js
@@ -3,7 +3,7 @@ import { Map } from 'react-amap';
 import { Box, Text } from 'grommet';
 
 export const CanvasContainer = ({ children, ...props }) => (
-  <Map plugins={['Scale']} version='1.4.15' {...props}>
+  <Map version='1.4.15' {...props}>
     {children}
     <Box style={{
       position: 'absolute',
